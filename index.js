5c75609bf840b962b2e566ecdf13ed3e7ea56b47
Monjurgithub <monjur1000980@gmail.com>
2023-05-03 12:44:03 +0600

    server is running and chef data send

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ let cors = require('cors')
 app.use(cors())
 const chefsData = require("./data/data.json")
 
-
+app.get('/', (req, res) => {
+  res.send('server is running!')
+})
+app.get("/chefs", (req, res) => {
+  res.send(chefsData.chefs);
+})
 
 
 app.listen(port, () => {
