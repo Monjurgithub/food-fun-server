69f47698b4eb3eb6aafbaaa143cb4a38d0a68541
Monjurgithub <monjur1000980@gmail.com>
2023-05-03 16:41:44 +0600

    data is working

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const port = 5000
 let cors = require('cors')
 app.use(cors())
 const chefsData = require("./data/data.json")
+const chefdetails = require("./data/singledata.json")
 
 app.get('/', (req, res) => {
   res.send('server is running!')
 })
 app.get("/chefs", (req, res) => {
   res.send(chefsData.chefs);
+})
+
+app.get("/chefs/:id", (req, res)=> {
+  const id = parseInt(req.params.id);
+  const chefdtls = chefdetails.chefs.filter(c => parseInt(c.id) === id)
+    res.send(chefdtls)
+
 })
 
 
